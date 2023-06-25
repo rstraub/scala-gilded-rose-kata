195753f4a88a9deda6c33dc9698dd41369d108cd
Roy Straub <roy@codecraftr.nl>
2023-06-25 21:47:07 +0200

    Partial algebraic data types (sulfuras/aged brie)

--- src/main/scala/nl/codecraftr/scala/gildedrose/StoreItem.scala
+++ src/main/scala/nl/codecraftr/scala/gildedrose/StoreItem.scala
@@ -1,21 +1,53 @@
 package nl.codecraftr.scala.gildedrose
 
+import nl.codecraftr.scala.gildedrose.StoreItem.{
+  AGED_BRIE,
+  BACKSTAGE_PASS,
+  MAX_QUALITY,
+  MIN_QUALITY,
+  SULFURAS
+}
+
+sealed trait StoreItem {
+  def name: String
+  def sellIn: Int
+  def quality: Int
+
+  def updatedQuality: StoreItem
+  def updatedSellBy: StoreItem
+}
+
+case class Sulfuras(sellIn: Int) extends StoreItem {
+  override def name: String = SULFURAS
+  override def quality: Int = 80
+
+  override def updatedQuality: StoreItem = this
+  override def updatedSellBy: StoreItem = this
+}
+
+case class AgedBrie(sellIn: Int, quality: Int) extends StoreItem {
+  override def name: String = AGED_BRIE
+
+  override def updatedQuality: StoreItem = {
+    val newQuality =
+      sellIn match {
+        case x if x < 0 => quality + 2
+        case _          => quality + 1
+      }
+
+    if (newQuality > MAX_QUALITY) copy(quality = MAX_QUALITY)
+    else copy(quality = newQuality)
+  }
+
+  override def updatedSellBy: StoreItem = copy(sellIn = sellIn - 1)
+}
+
 // D -> immutable
-case class StoreItem(name: String, sellIn: Int, quality: Int) {
-  private val MIN_QUALITY = 0
-  private val MAX_QUALITY = 50
-  private val SULFURAS = "Sulfuras, Hand of Ragnaros"
-  private val AGED_BRIE = "Aged Brie"
-  private val BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+// Only here until the types are correct
+case class TempItem(name: String, sellIn: Int, quality: Int) extends StoreItem {
 
   private def calculateNewQuality: Int = {
     name match {
-      case SULFURAS => quality
-      case AGED_BRIE =>
-        sellIn match {
-          case x if x < 0 => quality + 2
-          case _          => quality + 1
-        }
       case BACKSTAGE_PASS =>
         sellIn match {
           case x if x < 0   => 0
@@ -49,6 +81,16 @@ case class StoreItem(name: String, sellIn: Int, quality: Int) {
 }
 
 object StoreItem {
-  def from(item: Item): StoreItem =
-    StoreItem(item.name, item.sellIn, item.quality)
+  val MIN_QUALITY = 0
+  val MAX_QUALITY = 50
+  val SULFURAS = "Sulfuras, Hand of Ragnaros"
+  val AGED_BRIE = "Aged Brie"
+  val BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+  def from(item: Item): StoreItem = {
+    item.name match {
+      case SULFURAS => Sulfuras(item.sellIn)
+      case AGED_BRIE => AgedBrie(item.sellIn, item.quality)
+      case _        => TempItem(item.name, item.sellIn, item.quality)
+    }
+  }
 }
