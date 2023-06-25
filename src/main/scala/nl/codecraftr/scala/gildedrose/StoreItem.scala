d450f67c7fce5a86d228a1a055fa94ac65edfd18
Roy Straub <roy@codecraftr.nl>
2023-06-25 21:52:13 +0200

    Completed algebraic data types for store items

--- src/main/scala/nl/codecraftr/scala/gildedrose/StoreItem.scala
+++ src/main/scala/nl/codecraftr/scala/gildedrose/StoreItem.scala
@@ -8,6 +8,23 @@ import nl.codecraftr.scala.gildedrose.StoreItem.{
   SULFURAS
 }
 
+object StoreItem {
+  val MIN_QUALITY = 0
+  val MAX_QUALITY = 50
+  val SULFURAS = "Sulfuras, Hand of Ragnaros"
+  val AGED_BRIE = "Aged Brie"
+  val BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+
+  def from(item: Item): StoreItem = {
+    item.name match {
+      case SULFURAS       => Sulfuras(item.sellIn)
+      case BACKSTAGE_PASS => BackstagePass(item.sellIn, item.quality)
+      case AGED_BRIE      => AgedBrie(item.sellIn, item.quality)
+      case _              => RegularItem(item.name, item.sellIn, item.quality)
+    }
+  }
+}
+
 sealed trait StoreItem {
   def name: String
   def sellIn: Int
@@ -42,55 +59,38 @@ case class AgedBrie(sellIn: Int, quality: Int) extends StoreItem {
   override def updatedSellBy: StoreItem = copy(sellIn = sellIn - 1)
 }
 
-// D -> immutable
-// Only here until the types are correct
-case class TempItem(name: String, sellIn: Int, quality: Int) extends StoreItem {
+case class BackstagePass(sellIn: Int, quality: Int) extends StoreItem {
+  override def name: String = BACKSTAGE_PASS
 
-  private def calculateNewQuality: Int = {
-    name match {
-      case BACKSTAGE_PASS =>
-        sellIn match {
-          case x if x < 0   => 0
-          case x if x <= 5  => quality + 3
-          case x if x <= 10 => quality + 2
-          case _            => quality + 1
-        }
-      case _ =>
-        sellIn match {
-          case x if x < 0 => quality - 2
-          case _          => quality - 1
-        }
-    }
+  override def updatedQuality: StoreItem = {
+    val newQuality =
+      sellIn match {
+        case x if x < 0   => 0
+        case x if x <= 5  => quality + 3
+        case x if x <= 10 => quality + 2
+        case _            => quality + 1
+      }
+
+    if (newQuality > MAX_QUALITY) copy(quality = MAX_QUALITY)
+    else copy(quality = newQuality)
   }
 
-  def updatedQuality: StoreItem = {
-    if (name == SULFURAS) this
-    else {
-      val updatedQuality = calculateNewQuality
-
-      if (updatedQuality > MAX_QUALITY) copy(quality = MAX_QUALITY)
-      else if (updatedQuality < MIN_QUALITY) copy(quality = MIN_QUALITY)
-      else copy(quality = updatedQuality)
-    }
-  }
-
-  def updatedSellBy: StoreItem = {
-    if (name == SULFURAS) this
-    else copy(sellIn = sellIn - 1)
-  }
+  override def updatedSellBy: StoreItem = copy(sellIn = sellIn - 1)
 }
 
-object StoreItem {
-  val MIN_QUALITY = 0
-  val MAX_QUALITY = 50
-  val SULFURAS = "Sulfuras, Hand of Ragnaros"
-  val AGED_BRIE = "Aged Brie"
-  val BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
-  def from(item: Item): StoreItem = {
-    item.name match {
-      case SULFURAS => Sulfuras(item.sellIn)
-      case AGED_BRIE => AgedBrie(item.sellIn, item.quality)
-      case _        => TempItem(item.name, item.sellIn, item.quality)
+// D -> immutable
+case class RegularItem(name: String, sellIn: Int, quality: Int)
+    extends StoreItem {
+  def updatedQuality: StoreItem = {
+    val updatedQuality = sellIn match {
+      case x if x < 0 => quality - 2
+      case _          => quality - 1
     }
+
+    if (updatedQuality > MAX_QUALITY) copy(quality = MAX_QUALITY)
+    else if (updatedQuality < MIN_QUALITY) copy(quality = MIN_QUALITY)
+    else copy(quality = updatedQuality)
   }
+
+  def updatedSellBy: StoreItem = copy(sellIn = sellIn - 1)
 }
