2cb89c74bbc71f51426bc792e72e5b994e1475a3
Roy Straub <roy@codecraftr.nl>
2023-06-25 19:05:55 +0200

    Added self-checking acceptance test

--- src/test/scala/nl/codecraftr/scala/gildedrose/GildedRoseAcceptanceTest.scala
+++ src/test/scala/nl/codecraftr/scala/gildedrose/GildedRoseAcceptanceTest.scala
@@ -3,9 +3,9 @@ package nl.codecraftr.scala.gildedrose
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-object GildedRoseAcceptanceTest extends AnyFlatSpec with Matchers {
-  def main(args: Array[String]): Unit = {
-    val items = Array[Item](
+class GildedRoseAcceptanceTest extends AnyFlatSpec with Matchers {
+  "gilded rose" should "update inventory correctly" in {
+    val startingInventory = Array(
       new Item("+5 Dexterity Vest", 10, 20),
       new Item("Aged Brie", 2, 0),
       new Item("Elixir of the Mongoose", 5, 7),
@@ -13,20 +13,35 @@ object GildedRoseAcceptanceTest extends AnyFlatSpec with Matchers {
       new Item("Sulfuras, Hand of Ragnaros", -1, 80),
       new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
       new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)
       // this conjured item does not work properly yet
-      new Item("Conjured Mana Cake", 3, 6)
+//      new Item("Conjured Mana Cake", 3, 6)
     )
-    val app = new GildedRose(items)
-    val days = if (args.length > 0) args(0).toInt + 1 else 2
-    for (i <- 0 until days) {
-      System.out.println("-------- day " + i + " --------")
-      System.out.println("name, sellIn, quality")
-      for (item <- items) {
-        System.out.println(item.name + ", " + item.sellIn + ", " + item.quality)
+
+    val inventoryAfterTwoDays = Array(
+      new Item("+5 Dexterity Vest", 8, 18),
+      new Item("Aged Brie", 0, 2),
+      new Item("Elixir of the Mongoose", 3, 5),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 13, 22),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 8, 50),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 3, 50)
+      // this conjured item does not work properly yet
+//        new Item("Conjured Mana Cake", 3, 6)
+    )
+
+    val app = new GildedRose(startingInventory)
+
+    (0 until 2)
+      .foreach(_ => app.updateQuality())
+
+    startingInventory
+      .zip(inventoryAfterTwoDays)
+      .foreach { case (actual, expected) =>
+        actual.name shouldBe expected.name
+        actual.sellIn shouldBe expected.sellIn
+        actual.quality shouldBe expected.quality
       }
-      System.out.println()
-      app.updateQuality()
-    }
   }
 }
